2cb4ca0633333413807c1a0c66073646f7c35d8a
SeonyoungMin <alstjsdud2484@gmail.com>
2026-05-26 16:15:14 +0900

    불필요한 주석 정리

--- src/main/java/com/team404/repository/GeminiApiRepository.java
+++ src/main/java/com/team404/repository/GeminiApiRepository.java
@@ -4,7 +4,5 @@ import com.team404.domain.Image;
 
 public interface GeminiApiRepository {
 
-	// 이미지 + 프롬프트를 Gemini에 전달하고 응답 반환.
-	// 내부 인코딩(base64) 디테일은 구현체가 책임진다.
 	String analyze(Image image, String prompt);
 }
