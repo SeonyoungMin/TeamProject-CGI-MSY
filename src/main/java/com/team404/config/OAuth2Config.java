2f4dbd4a3fb03234624b0ebc8a41f2eddb36a92a
gainping <chlrkdls001@gamil.com>
2026-05-29 15:45:23 +0900

    챗봇 구현 중 컨트롤러까지 완성 / OAuth2Config 중요 키 application으로 이동 이그노어 없앰

---
+++ src/main/java/com/team404/config/OAuth2Config.java
@@ -0,0 +1,62 @@
+package com.team404.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+import com.team404.security.CustomOAuth2UserService;
+import com.team404.security.OAuth2SuccessHandler;
+import com.team404.service.UserService;
+
+@Configuration
+public class OAuth2Config {
+
+    @Autowired
+    private UserService userService;
+
+    @Value("${google.client.id}")
+    private String googleClientId;
+
+    @Value("${google.client.secret}")
+    private String googleClientSecret;
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(
+            googleClientRegistration()
+        );
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService(
+            ClientRegistrationRepository clientRegistrationRepository) {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+    }
+
+    @Bean
+    public CustomOAuth2UserService customOAuth2UserService() {
+        return new CustomOAuth2UserService();
+    }
+
+    @Bean
+    public OAuth2SuccessHandler oAuth2SuccessHandler() {
+        return new OAuth2SuccessHandler(userService);
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistrations.fromIssuerLocation("https://accounts.google.com")
+            .registrationId("google")
+            .clientId(googleClientId)
+            .clientSecret(googleClientSecret)
+            .redirectUri("{baseUrl}/login/oauth2/code/google")
+            .scope("openid", "profile", "email")
+            .build();
+    }
+}
